fce8544071905e07cbbc1e99d91733fd022dab88
Gustavo_Rallenson <rallenson900@gmail.com>
2022-10-28 20:19:33 -0300

    Feat:Finalizado isPrime

--- 05-isPrime/index.js
+++ 05-isPrime/index.js
@@ -1,4 +1,13 @@
 export function isPrime(value) {
   // implementar logica aqui
-  
+  if(value === 1){
+    return false
+  }else{
+    for(let divisor = 2; divisor < value; divisor++){
+      if((value%divisor === 0)){
+          return false
+      }
+    }
+  }
+  return true
 }
