bc26939af95b3794164adf9885f2bc5cd3c3c1c9
Gustavo_Rallenson <rallenson900@gmail.com>
2022-10-28 21:28:26 -0300

    Feat:Finalaizando SumEven

--- 07-sumEven/index.js
+++ 07-sumEven/index.js
@@ -1,4 +1,14 @@
 export function sumEven(value) {
   // implementar logica aqui
-  
+  let Soma = 0
+    for(let i = 0;i < value.length;i++){
+      if(value === undefined){
+        return 0
+      }else{
+        if(value[i] % 2 === 0){
+          Soma = value[i] + Soma
+        }
+    }
+  }
+  return Soma
 }
