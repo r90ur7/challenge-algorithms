d98388c5d26cab80f65b1b2d02839433f6bae6a1
Gustavo_Rallenson <rallenson900@gmail.com>
2022-10-28 21:44:37 -0300

    Feat:Finalizado isAnagram

--- 08-isAnagram/index.js
+++ 08-isAnagram/index.js
@@ -1,4 +1,20 @@
 export function isAnagram(word1, word2) {
   // implementar logica aqui
-  
+  const anagrama = word1.toLowerCase().split('');
+  const palavra =  word2.toLowerCase().split('');
+  palavra.sort((a, b) => {
+    return a.localeCompare(b);
+  })
+  anagrama.sort((a, b) => {
+    return a.localeCompare(b);
+  })
+  if( anagrama.length > palavra.length){
+    return false
+  }
+  for(let i = 0;i < word2.length; i++){
+    if(anagrama[i] !== palavra[i]){ 
+        return false;
+    }
+  }
+return true;
 }
